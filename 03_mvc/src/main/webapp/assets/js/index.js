2b27cdc353a0fd5b398215b7209eab56bca53ec9
Audrey <cw195505@gmail.com>
2024-03-07 15:48:01 +0900

    240307

--- 03_mvc/src/main/webapp/assets/js/index.js
+++ 03_mvc/src/main/webapp/assets/js/index.js
@@ -19,15 +19,15 @@ location.host /* http://localhost:8080 */
 
 location.href /* http://localhost:8080/mvc/getDate.do */
 
-  const getDateTime = ()=>{
-    const type = document.getElementById('type');
-    if(type.value === 'date'){
-      location.href = getContextPath() + '/getDate.do';
-    } else if(type.value === 'time'){
-      location.href === getContextPath() + '/getTime.do';
-    } else if(type.value === 'datetime'){
-      location.href === getContextPath() + '/getDateTime.do';
-    }
+const getDateTime = ()=>{
+  const type = document.getElementById('type');
+  if(type.value === 'date'){
+    location.href = getContextPath() + '/getDate.do';
+  } else if(type.value === 'time'){
+    location.href = getContextPath() + '/getTime.do';
+  } else if(type.value === 'datetime'){
+    location.href = getContextPath() + '/getDateTime.do';
   }
+}
   
   document.getElementById('btn').addEventListener('click', getDateTime);
